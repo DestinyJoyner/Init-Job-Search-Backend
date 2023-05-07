9083e8791a7f6360b6d018489dbf3980169d641a
Daniel-Mazzilli <danmazzilli@pursuit.org>
2023-05-07 13:00:28 -0400

    added show, create, update to userController

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const users = express.Router();
-const { getAllUsers } = require("../queries/users.js");
+const {
+  getAllUsers,
+  getUserByID,
+  createUser,
+  updateUser,
+} = require("../queries/users.js");
 
 // Index
 users.get("/", async (req, res) => {
@@ -11,6 +16,38 @@ users.get("/", async (req, res) => {
     res.status(500).json({
       error: "Server error",
     });
+  }
+});
+
+// Show
+users.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  const user = await getUserByID(id);
+  if (!user.message) {
+    res.status(200).json(user);
+  } else {
+    res.redirect("/not-found");
+  }
+});
+
+// Create
+users.post("/", async (req, res) => {
+  const newUser = await createUser(req.body);
+  if (!newUser.message) {
+    res.status(200).json(newUser);
+  } else {
+    res.redirect("/not-found");
+  }
+});
+
+// Update
+users.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  const updatedUser = await updateUser(req.body, id);
+  if (!updatedUser.message) {
+    res.status(200).json(updatedUser);
+  } else {
+    res.redirect("/not-found");
   }
 });
 
--- queries/users.js
+++ queries/users.js
@@ -9,8 +9,51 @@ const getAllUsers = async () => {
   }
 };
 
-// getUserByID, createUser, updateUser
+const getUserByID = async (userID) => {
+  try {
+    const oneUser = await db.one("SELECT * FROM users WHERE id=$1", userID);
+    return oneUser;
+  } catch (error) {
+    return error;
+  }
+};
+
+const createUser = async ({
+  first_name,
+  last_name,
+  school,
+  bio,
+  project_one,
+  project_two,
+}) => {
+  // conditionals for projects tbd
+  try {
+    const newUser = await db.one(
+      "INSERT INTO users (first_name, last_name, school, bio, project_one, project_two) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *",
+      [first_name, last_name, school, bio, project_one, project_two]
+    );
+    return newUser;
+  } catch (error) {
+    return error;
+  }
+};
+
+const updateUser = async (user, userID) => {
+  const { first_name, last_name, school, bio, project_one, project_two } = user;
+  try {
+    const updatedUser = await db.one(
+      "UPDATE users SET first_name=$1, last_name=$2, school=$3, bio=$4, project_one=$5, project_two=$6 WHERE id=$7 RETURNING *",
+      [first_name, last_name, school, bio, project_one, project_two, userID]
+    );
+    return updatedUser;
+  } catch (error) {
+    return error;
+  }
+};
 
 module.exports = {
   getAllUsers,
+  getUserByID,
+  createUser,
+  updateUser,
 };
