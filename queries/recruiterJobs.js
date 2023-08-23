675dc6b83596fa0c5add5c3b6673673681cdcec1
DestinyJoyner <destinyjoyner@pursuit.org>
2023-08-23 18:21:24 -0400

    users join command added for sort to return length of applicants fix

--- queries/recruiterJobs.js
+++ queries/recruiterJobs.js
@@ -6,6 +6,29 @@ const getRecruiterJobsSortOptions = async (recruiterID, sortOption = 'id') => {
             "SELECT title, company, city, full_remote, id from jobs WHERE recruiter_id=$1 ORDER BY $2:name",
             [recruiterID, sortOption]
           );
+
+          const recruiterJobsUsers = await db.any(
+            "SELECT job_id, users_jobs.user_id FROM users_jobs JOIN jobs ON jobs.id = users_jobs.job_id JOIN users ON users.id = users_jobs.user_id WHERE jobs.recruiter_id = $1",
+            recruiterID
+          );
+      
+          recruiterJobs.forEach(({ id }, i) =>
+            recruiterJobsUsers.forEach((e) => {
+              id === e["job_id"]
+                ? recruiterJobs[i].users !== undefined
+                  ? (recruiterJobs[i].users = [
+                      ...recruiterJobs[i].users,
+                      e["user_id"],
+                    ])
+                  : (recruiterJobs[i].users = [
+                      e["user_id"]
+                      
+                    ])
+                : recruiterJobs[i].users === undefined ?
+                recruiterJobs[i].users= [] : null;
+            })
+          );
+
         return recruiterJobs
     } catch (error) {
         return error
