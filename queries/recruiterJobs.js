8e899099109d36393f38caa6f42254adfdb1ddb1
DestinyJoyner <destinyjoyner@pursuit.org>
2024-04-08 00:04:00 -0400

    recruiter job query fix

--- queries/recruiterJobs.js
+++ queries/recruiterJobs.js
@@ -1,7 +1,7 @@
 const db = require("../db/dbConfig.js");
 
 const getRecruiterJobsSortOptions = async (recruiterID, sortOption = 'id') => {
-  const sortValue = sortOption === "idDesc" ? "id DESC" : `${sortOption}`
+  const sortValue = sortOption === "idDesc" ? "id DESC" : sortOption
     try {
         const recruiterJobs = await db.any(
             "SELECT title, company, city, full_remote, id from jobs WHERE recruiter_id=$1 ORDER BY $2",
